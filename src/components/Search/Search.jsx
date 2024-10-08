c990f302c5512cb47d5a5fec668adb55fe400ea4
Abdulhameed <anofiolawale@gmail.com>
2024-10-08 14:50:25 +0100

    cleared unnecesary data

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import searchIcon from '../../assets/circum_search.svg';
-import locationIcon from '../../assets/search_location_6.svg';
-import vectorIcon from '../../assets/search_vector_1.svg';
 import search from './Search.module.css';
 
 
@@ -12,7 +10,7 @@ function Search({setSearch, loading, mosques}) {
     const handleClick = () => {
         mosques();
     };
-    
+
     React.useEffect(() => {
         mosques();
     }, []);
