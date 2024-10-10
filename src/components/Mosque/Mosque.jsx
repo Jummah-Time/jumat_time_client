db750b60c99caf21a1e43d2be733d8a922ba0ca8
Abdulhameed <anofiolawale@gmail.com>
2024-10-10 20:37:05 +0100

    added 12hr time display format

--- src/components/Mosque/Mosque.jsx
+++ src/components/Mosque/Mosque.jsx
@@ -3,6 +3,19 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 
 function Mosques({mosques}) {
+    const convertTo12HourFormat = (time) => {
+        let [hours, minutes] = time.split(':');
+        hours = parseInt(hours);
+
+        if (hours >= 12) {
+            if (hours > 12) hours -= 12;
+        } else if (hours === 0) {
+            hours = 12;
+        }
+
+        return `${hours}:${minutes}`;
+    };
+
     return (
         <>
         <div>
@@ -15,7 +28,7 @@ function Mosques({mosques}) {
                                     {mosque.name}
                                 </h3>
                                 <p className={style.address}>{mosque.address}</p>
-                                <p className={style.time}>{mosque.jumat_start} - {mosque.jumat_end}</p>
+                                <p className={style.time}>{convertTo12HourFormat(mosque.jumat_start)} - {convertTo12HourFormat(mosque.jumat_end)}</p>
                                 <a href={mosque.link}>locate in map</a>
                             </li>
                         </ul>
